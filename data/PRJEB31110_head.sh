cbc2187ccf0a706b7a77da1b1dcd08e97317dc8b
Kailyn Hanke <kah357@cbsulm35.biohpc.cornell.edu>
2025-02-20 02:02:05 -0500

    First commit: .gitignore and ena files!

---
+++ data/PRJEB31110_head.sh
@@ -0,0 +1,4 @@
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR314/005/ERR3143465/ERR3143465_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR314/000/ERR3141400/ERR3141400_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR314/000/ERR3141540/ERR3141540_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR314/000/ERR3141470/ERR3141470_1.fastq.gz
