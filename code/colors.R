9f9f803a85196a25242ea4c13a0aa303059f4bcb
Kailyn Hanke <kah357@cbsulm35.biohpc.cornell.edu>
2025-04-11 11:53:21 -0400

    updating my colors.R

---
+++ code/colors.R
@@ -0,0 +1,8 @@
+# Setting colors for environment_(materials)
+environment_colors <- c(
+  "Bulk Tank Milk" = "dodgerblue4",
+  "Collection Tanker Milk" = "dodgerblue2",
+  "Skimmed Milk Powder" = "#D9CC3C",
+  "Skimmed Milk Silo" = "#A0E0BA",
+  "Whole Milk Silo" = "#00ADA7",
+  "Cream Silo" = "violetred")
